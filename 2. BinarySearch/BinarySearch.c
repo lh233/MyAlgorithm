7cb56a69c1409c8237b23ab9f3a5c746f817c99a
lh233 <490095224@qq.com>
2019-06-28 14:47:42 +0800

    增加二分查找法

---
+++ 2. BinarySearch/BinarySearch.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int binarySearch(int a[], int n, int key){
+    int low = 0;
+    int high = n - 1;
+
+    while(low<= high){
+        int mid = (low + high)/2;
+        int midVal = a[mid];
+        if(midVal<key)
+        low = mid + 1;
+        else if(midVal>key)
+        high = mid - 1;
+        else
+        return mid;
+    }
+    return -1;
+}
+
+
+int main()
+{
+    int i, val, ret;
+    int a[8] = {-32, 12, 16, 24, 36, 45, 59, 98};
+
+    for(i=0; i<8; i++)
+        printf("%d\t", a[i]);
+
+    printf("\n请输入所要查找的元素：");
+    scanf("%d", &val);
+
+    ret = binarySearch(a, sizeof(a)/sizeof(int), val);
+
+    if(-1 == ret)
+        printf("查找失败\n");
+    else
+        printf("查找成功,位置在第%d\n", ret + 1);
+    return 0;
+}
